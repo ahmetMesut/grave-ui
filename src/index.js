89aafff9390cd6b8f2b894b6c0ac884dfb7264c7
Ahmet Mesut Şahinoğlu <ahmet.sahinoglu@mebitech.com>
2016-11-01 15:47:48 +0300

    fixed.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = document.getElementById("app");
 
 // ****FOR DEV****
 window.applicationRootPath = "/";
-window.backendRootPath = "/grave";
+window.backendRootPath = "/grave/";
 
 
 render((<Switch />), app);
